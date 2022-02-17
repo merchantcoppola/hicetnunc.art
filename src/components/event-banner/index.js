0db28c4eeec73874e6269c97366d8d44262000e7
merchantcoppola <97635650+merchantcoppola@users.noreply.github.com>
2022-02-17 08:33:32 +0100

    call to Action

--- src/components/event-banner/index.js
+++ src/components/event-banner/index.js
@@ -6,7 +6,7 @@ export const EventBanner = () => {
         <div className={styles.event__banner}>
             <div className={styles.content}>
                 <h1>
-                The TEIA community has moved to teia.art!  <a className={styles.desktop__link} href="https://blog.teia.art/blog/teia-art-launch-announcement">Learn more</a>
+                The TEIA community has moved to teia.art; Please update your bookmarks and Twitter profiles!  <a className={styles.desktop__link} href="https://blog.teia.art/blog/teia-art-launch-announcement">Learn more</a>
                 </h1>
                 <a href="https://teia.art/" className={styles.content__button}>
                     teia.art
