0ec986796bff16b6f817c2319675f4f686fc3d91
merchantcoppola <97635650+merchantcoppola@users.noreply.github.com>
2022-02-23 12:02:35 +0100

    redirect to tranferred

--- src/components/event-banner/index.js
+++ src/components/event-banner/index.js
@@ -6,7 +6,7 @@ export const EventBanner = () => {
         <div className={styles.event__banner}>
             <div className={styles.content}>
                 <h1>
-                The TEIA community has moved to teia.art - hicetnunc.art will redirect to hicetnunc.xyz by the end of february.  <a className={styles.desktop__link} href="https://blog.teia.art/blog/teia-art-launch-announcement">Learn more</a>
+                The TEIA community has moved to teia.art - hicetnunc.art will be transferred to hicetnunc.xyz by the end of february.  <a className={styles.desktop__link} href="https://blog.teia.art/blog/teia-art-launch-announcement">Learn more</a>
                 </h1>
                 <a href="https://teia.art/" className={styles.content__button}>
                     teia.art
